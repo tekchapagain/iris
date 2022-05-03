59470e36eedbff7c8fc1997ba38b021e2b1c7d46
tekchapagain <tekchapagain1@gmail.com>
2022-05-03 21:27:38 +0545

    first commit

---
+++ predict/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from django.contrib import admin
+from predict.models import PredResults
+
+# Register your models here.
+admin.site.register(PredResults)
---
+++ predict/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'predict'
+
+urlpatterns = [
+    path('',views.predict, name='predict'),
+    path('predict/', views.predict_changes, name='submit_prediction'),
+    path('results/', views.view_results, name = 'results'),
+]
